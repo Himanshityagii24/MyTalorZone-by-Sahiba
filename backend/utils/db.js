6b492a7114b04ca99c1237e702e888385e37155b
Himanshityagii24 <tyagihimanshi241@gmail.com>
2024-12-23 15:05:26 +0530

    env updated

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        // Connect to MongoDB using MONGO_URI and NEW_PROJECT_DB from .env
         await mongoose.connect(`${process.env.MONGO_URI}/${process.env.NEW_PROJECT_DB}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
