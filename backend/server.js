50a7f9a7baa87be4e855036ca0c4fe562c382604
Himanshityagii24 <tyagihimanshi241@gmail.com>
2024-12-23 15:24:51 +0530

    cors policy fixed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./utils/db');
 const userRoutes = require('./routes/UserRoutes');
-const sellerRoutes = require('./routes/SellerRoutes')
+const sellerRoutes = require('./routes/SellerRoutes');
+const cors = require('cors');  // Import CORS middleware
 
 // Load environment variables
 dotenv.config();
@@ -11,6 +12,18 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://my-talor-zone-by-sahiba-9w49.vercel.app',  // Add your frontend URL here
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
+};
+
+// Use the CORS middleware with the specified options
+app.use(cors(corsOptions));
+
 app.use(express.json()); // Middleware to parse JSON request body
 
 // Routes
@@ -21,8 +34,8 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello from Vercel!');
-  });
+});
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
